591603f7f80d550ebc60cfdcc77dbed21fe07ca7
ManuelSuarez07 <manuel.suarez.e09@gmail.com>
2024-02-07 13:07:57 -0500

    Agregar proyecto 1 maven

---
+++ src/main/java/edu/eci/cvds/patterns/App.java
@@ -0,0 +1,12 @@
+package edu.eci.cvds.patterns;
+
+public class App {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, world!");
+        } else {
+            System.out.println("Hello, " + args[0] + "!");
+        }
+    }
+}
+ 
